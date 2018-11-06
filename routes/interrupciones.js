f08d91ca1858247fbf879a4ec67dbd2cc029aced
Alexis Crespo <metal@srjdb.metal>
2018-11-05 20:52:19 -0500

    seleccion de elementos por pg añadido

--- routes/interrupciones.js
+++ routes/interrupciones.js
@@ -7,14 +7,20 @@ var cliente = new Client(process.env.POSTGRES_URI);
 cliente.connect();
 
 router.post('/inter', function(req, res) {
-  var name = req.body;
 
+  let datos = req.body;
+  let pagina = datos[0];
+  let elementosPagina = datos[1];
+  let campOrden = datos[2];
+  let orden = datos[3];
+  let ultimo = datos[4];
+  console.log(datos)
   let qq = `SELECT row_to_json(conteo) FROM(SELECT COUNT(*) as total FROM interrupcion) as conteo
             UNION ALL
             SELECT row_to_json(fc)
             FROM (SELECT array_agg(f) As interrupciones
-            FROM (SELECT * FROM interrupcion WHERE area > 'Cuenca' ORDER BY area ASC
-            LIMIT 3) As f) As fc`;
+            FROM (SELECT * FROM interrupcion WHERE ${campOrden} > ${ultimo} ORDER BY ${campOrden} ${orden}
+            LIMIT ${elementosPagina}) As f) As fc`;
   var query = cliente.query(new Query(qq));
 
   query.on("row", function(row, result) {
@@ -34,7 +40,7 @@ router.post('/inter', function(req, res) {
   });
 
 });
-
+/*
 router.get('/opInt', function(req, res) {
 
   let qq = `SELECT row_to_json(fc)
@@ -62,6 +68,6 @@ router.get('/opInt', function(req, res) {
       }
     });
   });
-});
+});*/
 
 module.exports = router;
