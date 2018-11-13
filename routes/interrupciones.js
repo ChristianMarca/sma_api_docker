a723ceb98b398b6ab6e371b4f01e42114f882c4d
Alexis Crespo <metal@srjdb.metal>
2018-11-13 16:43:51 -0500

    adicion de paginacion

--- routes/interrupciones.js
+++ routes/interrupciones.js
@@ -9,18 +9,16 @@ cliente.connect();
 router.post('/inter', function(req, res) {
 
   let datos = req.body;
-  let pagina = datos[0];
+  let fetchOffset = datos[0];
   let elementosPagina = datos[1];
-  let campOrden = datos[2];
-  let orden = datos[3];
-  let ultimo = datos[4];
-  console.log(datos)
+  let orden = datos[2];
+  let campOrden = datos[3];
   let qq = `SELECT row_to_json(conteo) FROM(SELECT COUNT(*) as total FROM interrupcion) as conteo
             UNION ALL
             SELECT row_to_json(fc)
             FROM (SELECT array_agg(f) As interrupciones
-            FROM (SELECT * FROM interrupcion WHERE ${campOrden} > ${ultimo} ORDER BY ${campOrden} ${orden}
-            LIMIT ${elementosPagina}) As f) As fc`;
+            FROM (SELECT * FROM interrupcion WHERE area LIKE 'cue' ORDER BY ${campOrden} ${orden}
+            LIMIT ${elementosPagina} OFFSET ${fetchOffset}) As f) As fc`;
   var query = cliente.query(new Query(qq));
 
   query.on("row", function(row, result) {
@@ -30,15 +28,18 @@ router.post('/inter', function(req, res) {
   query.on("end", function(result) {
     let {total} = result.rows[0].row_to_json;
     let {interrupciones} = result.rows[1].row_to_json;
-    res.json({
-      title: "Express API",
-      jsonData: {
-        total,
-        interrupciones
-      }
-    });
+    res.json({total,interrupciones});
   });
 
 });
 
 module.exports = router;
+
+/*
+let qq = `SELECT row_to_json(conteo) FROM(SELECT COUNT(*) as total FROM interrupcion) as conteo
+          UNION ALL
+          SELECT row_to_json(fc)
+          FROM (SELECT array_agg(f) As interrupciones
+          FROM (SELECT * FROM interrupcion WHERE id_inte > ${fetchMark} ORDER BY id_inte ${fetchOrder}
+          LIMIT ${elementosPagina}) As f) As fc`;
+          */
