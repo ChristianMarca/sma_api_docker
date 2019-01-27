f63cf645da89aa2904f01adbcce772ca54d3986b
ChristianMarca <cmarcag@hotmail.com>
2019-01-27 15:12:53 -0500

    Configure Cors

--- app.js
+++ app.js
@@ -57,6 +57,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 // app.use(cors(corsOptions));
+
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 app.use(fileUpload());
 app.use((req, res, next) => {
 	res.io = io;
