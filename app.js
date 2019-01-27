98c4428691d30d3702fb472bfb4d5166e8ca02ad
ChristianMarca <cmarcag@hotmail.com>
2019-01-27 14:09:51 -0500

    Disable CORS

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(cors());
-app.use(cors(corsOptions));
+app.use(cors());
+// app.use(cors(corsOptions));
 app.use(fileUpload());
 app.use((req, res, next) => {
 	res.io = io;
