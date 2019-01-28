4c0692d209d060ac295d3ed975c16ffc0657adbf
ChristianMarca <cmarcag@hotmail.com>
2019-01-28 11:02:34 -0500

    Test for MailGun

--- services/email.js
+++ services/email.js
@@ -1,26 +1,40 @@
 var nodemailer = require('nodemailer');
+const mailgunTransport = require('nodemailer-mailgun-transport');
 var smtpTransport = require('nodemailer-smtp-transport');
 var inLineCss = require('nodemailer-juice');
 require('dotenv').load();
 
-var transporter = nodemailer.createTransport(
-	smtpTransport({
-		service: 'gmail',
-		host: 'smtp.gmail.com',
-		port: 587,
-		starttls: {
-			enable: true
-		},
-		secureConnection: true,
-		auth: {
-			user: process.env.EMAIL_GMAIL,
-			pass: process.env.PASSWORD_GMAIL
-		},
-		tls: {
-			rejectUnauthorized: false
-		}
-	})
-);
+// Configure transport options
+const mailgunOptions = {
+	auth: {
+		api_key: process.env.MAILGUN_ACTIVE_API_KEY,
+		domain: process.env.MAILGUN_DOMAIN
+	}
+};
+
+const transport = mailgunTransport(mailgunOptions);
+
+// var transporter = nodemailer.createTransport(
+// 	smtpTransport({
+// 		service: 'gmail',
+// 		host: 'smtp.gmail.com',
+// 		port: 587,
+// 		starttls: {
+// 			enable: true
+// 		},
+// 		secureConnection: true,
+// 		secure: true,
+// 		auth: {
+// 			user: process.env.EMAIL_GMAIL,
+// 			pass: process.env.PASSWORD_GMAIL
+// 		},
+// 		tls: {
+// 			rejectUnauthorized: false
+// 		}
+// 	})
+// );
+
+var transporter = nodemailer.createTransport(transport);
 
 _sendMail = (
 	from = '<test.sma.app@gmail.com>',
@@ -49,7 +63,7 @@ _sendMail = (
 	return new Promise((resolve, reject) => {
 		transporter.sendMail(mailOptions, function(error, info) {
 			if (error) {
-				console.log({Error:error})
+				console.log({ Error: error });
 				reject(error);
 			} else {
 				console.log('Email sent: ' + info.response);
